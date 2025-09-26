a45a5710d4f4b51f7ae3d4e2094c827f6f18eb46
AdityaChopra-raj <chopraadee2@gmail.com>
2025-09-26 11:32:40 +0530

    Update and rename requirements.txt to events_data.py

---
+++ events_data.py
@@ -0,0 +1,7 @@
+# Event data with images
+events = {
+    "Navratri Pooja": {"capacity": 150, "tickets_scanned": 0, "image": "images/navratri.jpg"},
+    "Diwali Dance": {"capacity": 150, "tickets_scanned": 0, "image": "images/diwali.jpg"},
+    "Freshers": {"capacity": 150, "tickets_scanned": 0, "image": "images/freshers.jpg"},
+    "Ravan Dehan": {"capacity": 150, "tickets_scanned": 0, "image": "images/ravan.jpg"}
+}
