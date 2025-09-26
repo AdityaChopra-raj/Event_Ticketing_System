8d9c901ca72737c11c4a5b6e9a7ca9d4292ee217
AdityaChopra-raj <chopraadee2@gmail.com>
2025-09-26 11:54:39 +0530

    Update blockchain.py

--- blockchain.py
+++ blockchain.py
@@ -5,19 +5,36 @@ class Blockchain:
         self.chain = []
         self.pending_transactions = []
 
-    def add_transaction(self, sender, receiver, ticket_id, event_name, scanned=False):
-        tx = {"sender": sender, "receiver": receiver,
-              "ticket_id": ticket_id, "event_name": event_name,
-              "scanned": scanned, "timestamp": time.time()}
+    def add_transaction(self, sender, receiver, ticket_id, event_name, customer_name=None, phone=None, uid=None, scanned=False):
+        tx = {
+            "sender": sender,
+            "receiver": receiver,
+            "ticket_id": ticket_id,
+            "event_name": event_name,
+            "customer_name": customer_name,
+            "phone": phone,
+            "uid": uid,
+            "scanned": scanned,
+            "timestamp": time.time()
+        }
         self.pending_transactions.append(tx)
         return tx
 
     def mine_block(self):
-        block = {"index": len(self.chain)+1,
-                 "timestamp": time.time(),
-                 "transactions": self.pending_transactions.copy(),
-                 "previous_hash": self.chain[-1]["hash"] if self.chain else "0"}
+        block = {
+            "index": len(self.chain)+1,
+            "timestamp": time.time(),
+            "transactions": self.pending_transactions.copy(),
+            "previous_hash": self.chain[-1]["hash"] if self.chain else "0"
+        }
         block["hash"] = str(hash(str(block)))
         self.chain.append(block)
         self.pending_transactions = []
         return block
+
+    def find_ticket(self, ticket_id):
+        for block in self.chain:
+            for tx in block["transactions"]:
+                if tx["ticket_id"] == ticket_id:
+                    return tx
+        return None
